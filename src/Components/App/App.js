1bd25ea2ef8b6612c95cfc643ee4d9f0bc414920
EggStew <edward.cstewart@btopenworld.com>
2023-08-18 13:52:10 +0100

    modified:   README.md 	modified:   public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   public/manifest.json 	deleted:    public/robots.txt 	modified:   src/Components/App/App.js 	new file:   src/Components/ConnectBtn/ConnectBtn.css 	new file:   src/Components/ConnectBtn/ConnectBtn.js modified:   src/Components/Playlist/Playlist.css 	modified:   src/Components/Playlist/Playlist.js 	modified:   src/Components/SearchBar/SearchBar.css 	modified:   src/Components/SearchBar/SearchBar.js 	modified:   src/Components/SearchResults/SearchResults.css 	modified:   src/Components/SearchResults/SearchResults.js 	new file:   src/Components/Track/Track.css 	modified:   src/Components/Track/Track.js 	new file:   src/Components/Tracklist/TrackList.css 	modified:   src/Components/Tracklist/Tracklist.js 	deleted:    src/index.js 	renamed:    src/index.css -> src/utils/index.css 	renamed:    src/reportWebVitals.js -> src/utils/reportWebVitals.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,108 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
-import Playlist from '../Playlist/Playlist';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
-import Track from '../Track/Track';
-import Tracklist from '../Tracklist/Tracklist';
+import Playlist from '../Playlist/Playlist';
+import ConnectBtn from '../ConnectBtn/ConnectBtn';
+import Spotify from '../../util/Spotify';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "searchResults": [],
+      "playlistName": "New Playlist",
+      "playlistTracks": [],
+      "connected": '',
+      "profileImage": ''
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+    this.connect = this.connect.bind(this);
+    this.disconnect = this.disconnect.bind(this);
+  }
+
+  componentWillMount() {
+    const newAccessToken = window.location.href.match(/access_token=([^&]*)/);
+    const newExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+    if (newAccessToken && newExpiresIn) {
+      this.connect();
+    }
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (!tracks.find(trackIndex => trackIndex.id === track.id)) {
+      tracks.push(track);
+      this.setState({playlistTracks: tracks});
+    }
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    let newTracks = tracks.filter(trackIndex => trackIndex.id !== track.id);
+    this.setState({playlistTracks: newTracks});
+
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  connect() {
+    Spotify.connect().then(response => {
+      if(response.id) {
+        this.setState({
+          connected: response.display_name,
+          profileImage: response.images[0].url
+        });
+      }
+    });
+  }
+
+  disconnect() {
+    Spotify.disconnect();
+    window.location.reload();
+  }
+
+  savePlaylist() {
+    let tracks = this.state.playlistTracks;
+    if(tracks.length && this.state.playlistName) {
+      let trackURIs = tracks.map(trackIndex => trackIndex.uri);
+      Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+        this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        });
+        document.getElementById('Playlist-name').value = this.state.playlistName;
+      });
+    }
+  }
+
+  search(searchTerm) {
+    Spotify.search(searchTerm).then(results => {
+      this.setState({searchResults: results});
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Ja<span className='highlight'>mm</span>ing</h1>
-        <div className='App'>
-          <SearchBar />
-          <div className='App-playlist'>
-            <SearchResults />
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <ConnectBtn onConnect={this.connect} onDisconnect={this.disconnect} connected={this.state.connected} imageUrl={this.state.profileImage}/>
+          <SearchBar onSearch={this.search} connected={this.state.connected}/>
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
 
-function Tracklist() {
-  return (
-    <>
-    </>
-  );
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map(track => <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />)}
+      </div>
+    );
+  }
 }
 
-export default Tracklist;
+export default TrackList;
